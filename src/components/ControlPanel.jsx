2455ff8a4b804711082a100a5ee43fdbe8619b91
Dowsley <joao.dowsley850@gmail.com>
2021-11-17 16:03:03 -0300

    feio mas tudo completo

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -4,7 +4,7 @@ import HouseLights from './cards/HouseLights'
 import Temperature from './cards/Temperature'
 
 import { initializeApp } from 'firebase/app'
-import { getDatabase, ref as fireRef } from "firebase/database"
+import { getDatabase, set, ref as fireRef } from "firebase/database"
 import { useObjectVal } from 'react-firebase-hooks/database'
 
 const app = initializeApp({
@@ -21,7 +21,14 @@ const app = initializeApp({
 const db = getDatabase(app)
 
 const ControlPanel = () => {
-  const [isLightsOn, loading, error] = useObjectVal(fireRef(db, 'LED'))
+  const [isLightsOn, lightLoading, lightError] = useObjectVal(fireRef(db, 'LED'))
+  const [isDoorClosed, doorLoading, doorError] = useObjectVal(fireRef(db, 'DOOR'))
+  const [isAlarmOn, alarmLoading, alarmError] = useObjectVal(fireRef(db, 'ALARM'))
+  const [temp, tempLoading, tempError] = useObjectVal(fireRef(db, 'TEMP'))
+
+  const toggle = (name, val) => {
+    set(fireRef(db, name), val ? 0 : 1)
+  }
 
   return (
     <>
@@ -35,20 +42,77 @@ const ControlPanel = () => {
 
           <div class="flex flex-wrap">
 
+
             <div class="w-full md:w-1/2 xl:w-1/2 p-6">
               <div class="bg-gradient-to-b from-yellow-200 to-yellow-100 border-b-4 border-yellow-600 rounded-lg shadow-xl p-5">
                 <div class="flex flex-row items-center">
                   <div class="flex-shrink pr-4">
-                    <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-user-plus fa-2x fa-inverse"></i></div>
+                    <button
+                      onClick={() => toggle('LED', isLightsOn)}
+                      className="material-icons bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+                    >
+                      power_settings_new
+                    </button>
                   </div>
                   <div class="flex-1 text-right md:text-center">
                     <h5 class="font-bold uppercase text-gray-600">House Lights</h5>
-                    <h3 class="font-bold text-3xl">{isLightsOn}</h3>
+                    <h3 class="font-bold text-3xl">{isLightsOn ? 'ON' : 'OFF'}</h3>
                   </div>
                 </div>
               </div>
             </div>
 
+
+            <div class="w-full md:w-1/2 xl:w-1/2 p-6">
+              <div class="bg-gradient-to-b from-pink-200 to-pink-100 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                  <div class="flex-shrink pr-4">
+                    <button
+                      onClick={() => toggle('DOOR', isDoorClosed)}
+                      className="material-icons bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+                    >
+                      power_settings_new
+                    </button>
+                  </div>
+                  <div class="flex-1 text-right md:text-center">
+                    <h5 class="font-bold uppercase text-gray-600">Front Door</h5>
+                    <h3 class="font-bold text-3xl">{isDoorClosed ? 'CLOSED' : 'OPEN'}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="w-full md:w-1/2 xl:w-1/2 p-6">
+              <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                  <div class="flex-shrink pr-4">
+                    <button
+                      onClick={() => toggle('ALARM', isAlarmOn)}
+                      className="material-icons bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+                    >
+                      power_settings_new
+                    </button>
+                  </div>
+                  <div class="flex-1 text-right md:text-center">
+                    <h5 class="font-bold uppercase text-gray-600">ALARM CLOCK</h5>
+                    <h3 class="font-bold text-3xl">{isAlarmOn ? 'ACTIVATED' : 'DEACTIVATED'}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="w-full md:w-1/2 xl:w-1/2 p-6">
+              <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4 border-blue-500 rounded-lg shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                  <div class="flex-1 text-right md:text-center">
+                    <h5 class="font-bold uppercase text-gray-600">Temperature</h5>
+                    <h3 class="font-bold text-3xl">{temp} °C</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
 
 
           </div>
